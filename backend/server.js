3c6bf6bc9bc2679a9a61e1a801ac567f202bf50b
iamsam15 <134360554+iamsam15@users.noreply.github.com>
2025-02-13 21:55:36 +0530

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fs from "node:fs";
+import connect from "./src/db/connect.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+const routeFiles = fs.readdirSync("./src/routes");
+
+routeFiles.forEach((file) => {
+  import(`./src/routes/${file}`)
+    .then((route) => {
+      app.use("/api/v1", route.default);
+    })
+    .catch((err) => {
+      console.log("Failed to load route files", err);
+    });
+});
+
 const server = async () => {
-  console.log(`Server is running`);
+  try {
+    await connect();
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to start Server...", error.message);
+    process.exit(1);
+  }
 };
 server();
